b1befd633537ae23207d326f5ecb53cbcc61a85b
HDI1234 <hasanaldaniel@gmail.com>
2021-03-02 08:29:11 +0800

    private info

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -3,13 +3,8 @@ $config = array(
     'protocol' => 'smtp',
     'smtp_host' => 'smtp.gmail.com', 
     'smtp_port' => 465,
-<<<<<<< Updated upstream
-    'smtp_user' => 'hasanaldaniel@gmail.com',
-    'smtp_pass' => '123diamond',
-=======
-    'smtp_user' => 'hasanaldaniel@gmail.com', //gmail acc
-    'smtp_pass' => '123diamond', //gmail password
->>>>>>> Stashed changes
+    'smtp_user' => '', //gmail acc
+    'smtp_pass' => '', //gmail password
     'smtp_crypto' => 'ssl',
     'mailtype' => 'html',
     'smtp_timeout' => '4', 
